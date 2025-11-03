b161abbbdf23b18139b009b0c23bc8dc1fd077d6
Brayden Lilienthal <Lilienthalbrayden@gmail.com>
2025-11-03 00:30:09 -0600

    Script detects collision with Vinyls and vinyl player
    Script does that and activates audio after a delay. repeats as long as it is active

--- Assets/Record_player/Scripts/RecordPlayer.cs
+++ Assets/Record_player/Scripts/RecordPlayer.cs
@@ -1,101 +1,119 @@
 using UnityEngine;
 using System.Collections;
 
-public class RecordPlayer : MonoBehaviour {
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-
+public class RecordPlayer : MonoBehaviour
+{
     public bool recordPlayerActive = false;
 
     GameObject disc;
     GameObject arm;
+
+    // External vinyl object (assign in Inspector)
+    public GameObject vinyl;
+
+    // Audio source for playing music (assign in Inspector)
+    public AudioSource audioSource;
 
     int mode;
     float armAngle;
     float discAngle;
     float discSpeed;
 
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-void Awake()
-{
-    disc = gameObject.transform.Find("teller").gameObject;
-    arm = gameObject.transform.Find("arm").gameObject;
-}
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-void Start()
-{
-    mode = 0;
-    armAngle = 0.0f;
-    discAngle = 0.0f;
-    discSpeed = 0.0f;
-}
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-void Update()
-{
-    //-- Mode 0: player off
-    if(mode == 0)
-    {   
-        if(recordPlayerActive == true)
-            mode = 1;
-    }
-    //-- Mode 1: activation
-    else if(mode == 1)
+    void Awake()
     {
-        if(recordPlayerActive == true)
+        disc = gameObject.transform.Find("teller").gameObject;
+        arm = gameObject.transform.Find("arm").gameObject;
+    }
+
+    void Start()
+    {
+        mode = 0;
+        armAngle = 0.0f;
+        discAngle = 0.0f;
+        discSpeed = 0.0f;
+    }
+
+    void Update()
+    {
+        // Mode 0: player off
+        if (mode == 0)
         {
-            armAngle += Time.deltaTime * 30.0f;
-            if(armAngle >= 30.0f)
+            if (recordPlayerActive)
+                mode = 1;
+        }
+        // Mode 1: arm moving out / activation
+        else if (mode == 1)
+        {
+            if (recordPlayerActive)
             {
-                armAngle = 30.0f;
-                mode = 2;
+                armAngle += Time.deltaTime * 30.0f;
+                if (armAngle >= 30.0f)
+                {
+                    armAngle = 30.0f;
+                    mode = 2; // arm fully moved, start spinning
+                }
+                discAngle += Time.deltaTime * discSpeed;
+                discSpeed += Time.deltaTime * 80.0f;
             }
-            discAngle += Time.deltaTime * discSpeed;
-            discSpeed += Time.deltaTime * 80.0f;
+            else
+            {
+                mode = 3; // stop mode if turned off
+            }
         }
-        else
-            mode = 3;
-    }
-    //-- Mode 2: running
-    else if(mode == 2)
-    {
-        if(recordPlayerActive == true)
-            discAngle += Time.deltaTime * discSpeed;
-        else
-            mode = 3;
-    }
-    //-- Mode 3: stopping
-    else
-    {
-        if(recordPlayerActive == false)
+        // Mode 2: running
+        else if (mode == 2)
         {
-            armAngle -= Time.deltaTime * 30.0f;
-            if(armAngle <= 0.0f)
-                armAngle = 0.0f;
-
-            discAngle += Time.deltaTime * discSpeed;
-            discSpeed -= Time.deltaTime * 80.0f;
-            if(discSpeed <= 0.0f)
-                discSpeed = 0.0f;
-
-            if((discSpeed == 0.0f) && (armAngle == 0.0f))
-                mode = 0;
+            if (recordPlayerActive)
+                discAngle += Time.deltaTime * discSpeed;
+            else
+                mode = 3;
         }
-        else
-            mode = 1;
-    }
+        // Mode 3: stopping
+        else // mode == 3
+        {
+            if (!recordPlayerActive)
+            {
+                armAngle -= Time.deltaTime * 30.0f;
+                if (armAngle <= 0.0f)
+                    armAngle = 0.0f;
 
-    //-- update objects
-    arm.transform.localEulerAngles = new Vector3(0.0f, armAngle, 0.0f);
-    disc.transform.localEulerAngles = new Vector3(0.0f, discAngle, 0.0f);
-}
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
-//--------------------------------------------------------------------------------------------
+                discAngle += Time.deltaTime * discSpeed;
+                discSpeed -= Time.deltaTime * 80.0f;
+                if (discSpeed <= 0.0f)
+                    discSpeed = 0.0f;
+
+                if (discSpeed == 0.0f && armAngle == 0.0f)
+                    mode = 0;
+            }
+            else
+            {
+                mode = 1;
+            }
+        }
+
+        // Update arm and disc rotation
+        arm.transform.localEulerAngles = new Vector3(0.0f, armAngle, 0.0f);
+        disc.transform.localEulerAngles = new Vector3(0.0f, discAngle, 0.0f);
+
+        // Rotate vinyl around world Y axis while spinning
+        if (vinyl != null && recordPlayerActive && discSpeed > 0.0f)
+        {
+            vinyl.transform.Rotate(Vector3.up, discSpeed * Time.deltaTime, Space.World);
+        }
+
+        // Audio control: play only when arm is fully moved and spinning (mode 2)
+        if (audioSource != null)
+        {
+            if (mode == 2 && recordPlayerActive && discSpeed > 0.1f)
+            {
+                if (!audioSource.isPlaying)
+                    audioSource.Play();
+            }
+            else
+            {
+                if (audioSource.isPlaying)
+                    audioSource.Stop();
+            }
+        }
+    }
 }
